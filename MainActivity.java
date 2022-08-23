6f480be69059ca60d3aaf689d448df1a82a5568c
JINHEE PARK <100392709+JJinne@users.noreply.github.com>
2022-08-23 13:28:11 +0900

    Project_4_1_SimpleCalculator

---
+++ MainActivity.java
@@ -0,0 +1,103 @@
+package com.androidpark.project4_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv,btnRem;
+    TextView textResult;
+    String num1, num2;
+    Integer result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("초간단 계산기");
+
+        edit1 = (EditText) findViewById(R.id.edit1);
+        edit2 = (EditText) findViewById(R.id.edit2);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMul = (Button) findViewById(R.id.btnMul);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+        btnRem = (Button) findViewById(R.id.btnRem);
+
+        textResult = (TextView) findViewById(R.id.textResult);
+
+        btnAdd.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) + Integer.parseInt(num2);
+                textResult.setText("계산결과:" + result.toString());
+                return false;
+            }
+        });
+
+        btnSub.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) - Integer.parseInt(num2);
+                textResult.setText("계산결과:" + result.toString());
+                return false;
+            }
+        });
+        btnMul.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) * Integer.parseInt(num2);
+                textResult.setText("계산결과:" + result.toString());
+                return false;
+            }
+        });
+        btnDiv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) / Integer.parseInt(num2);
+                textResult.setText("계산결과:" + result.toString());
+                return false;
+            }
+        });
+        btnRem.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+
+                if(num1.getBytes().length <= 0){
+                    Toast.makeText(getApplicationContext(), "값을 입력하세요",Toast.LENGTH_SHORT).show();
+                }else{
+                    if(num2.equals("0")){
+                        Toast.makeText(getApplicationContext(), "0을 제외한 정수를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    }else{
+                        result = Integer.parseInt(num1) % Integer.parseInt(num2);
+                        textResult.setText("계산결과:" + result.toString());
+                    }
+                }
+
+                return false;
+            }
+        });
+
+
+    }
+
+}
